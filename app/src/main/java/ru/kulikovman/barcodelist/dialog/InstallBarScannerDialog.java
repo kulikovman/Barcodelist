76bf6c1e0e24e024ef2c720e40395473e96943b9
Maksim Kulikov <kulikovman@gmail.com>
2018-05-30 12:12:38 +0500

    Переадресация на страницу сканера в маркете

--- app/src/main/java/ru/kulikovman/barcodelist/dialog/InstallBarScannerDialog.java
+++ app/src/main/java/ru/kulikovman/barcodelist/dialog/InstallBarScannerDialog.java
@@ -2,6 +2,8 @@ package ru.kulikovman.barcodelist.dialog;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
@@ -19,7 +21,10 @@ public class InstallBarScannerDialog extends DialogFragment {
                 .setPositiveButton(R.string.install_bar_scanner_ok, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        // Ничего не делаем при нажатии
+                        // Переадресация на страницу приложения в маркете
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse(getString(R.string.zxing_market_link)));
+                        startActivity(intent);
                     }
                 })
                 .setNegativeButton(R.string.install_bar_scanner_cancel, new DialogInterface.OnClickListener() {
