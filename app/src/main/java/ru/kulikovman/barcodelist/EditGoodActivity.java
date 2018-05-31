b7b5cbdf519ea04d7f437f51ab853a12a775839c
Maksim Kulikov <kulikovman@gmail.com>
2018-05-31 20:25:58 +0500

    Добавлена активити под редактирование товара

---
+++ app/src/main/java/ru/kulikovman/barcodelist/EditGoodActivity.java
@@ -0,0 +1,13 @@
+package ru.kulikovman.barcodelist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditGoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_good);
+    }
+}
