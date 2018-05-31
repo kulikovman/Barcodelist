f4121a8c03757ef2229a6be8ebd43287b9f6aef5
Maksim Kulikov <kulikovman@gmail.com>
2018-05-31 23:42:46 +0500

    Изменение модели товара

--- app/src/main/java/ru/kulikovman/barcodelist/model/Good.java
+++ app/src/main/java/ru/kulikovman/barcodelist/model/Good.java
@@ -12,14 +12,14 @@ public class Good extends RealmObject {
     @PrimaryKey
     private long mId;
 
+    private String mBarcode;
     private String mName;
-    private long mBarcode;
     private String mGroup;
 
-    public Good(String name, long barcode, String group) {
+    public Good(String name, String barcode, String group) {
         mId = System.currentTimeMillis();
-        mName = name;
         mBarcode = barcode;
+        mName = name;
         mGroup = group;
     }
 
@@ -34,20 +34,20 @@ public class Good extends RealmObject {
         mId = id;
     }
 
+    public String getBarcode() {
+        return mBarcode;
+    }
+
+    public void setBarcode(String barcode) {
+        mBarcode = barcode;
+    }
+
     public String getName() {
         return mName;
     }
 
     public void setName(String name) {
         mName = name;
-    }
-
-    public long getBarcode() {
-        return mBarcode;
-    }
-
-    public void setBarcode(long barcode) {
-        mBarcode = barcode;
     }
 
     public String getGroup() {
